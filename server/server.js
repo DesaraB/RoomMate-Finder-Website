b125a0a664fdd11e3a561982404a3cee805c407c
DesaraB <desarabaroti99@gmail.com>
2025-05-16 16:50:22 +0200

    changed port

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const sequelize = require('./config/database');
-const User = require('./models/User');
-const Home = require('./models/Home');
-const Interest = require('./models/Interest');
+const express = require("express");
+const dotenv = require("dotenv");
+const sequelize = require("./config/database");
+const User = require("./models/User");
+const Home = require("./models/Home");
+const Interest = require("./models/Interest");
 
 // ⬇️ ADD THESE LINES TO IMPORT ROUTES
-const userRoutes = require('./routes/userRoutes');
-const homeRoutes = require('./routes/homeRoutes');
-const interestRoutes = require('./routes/interestRoutes');
+const userRoutes = require("./routes/userRoutes");
+const homeRoutes = require("./routes/homeRoutes");
+const interestRoutes = require("./routes/interestRoutes");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-
 // ⬇️ ADD ROUTE MIDDLEWARES
-app.use('/api/users', userRoutes);
-app.use('/api/homes', homeRoutes);
-app.use('/api/interests', interestRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/homes", homeRoutes);
+app.use("/api/interests", interestRoutes);
 
 // Sync models
-sequelize.sync({ alter: true }) // change to { force: true } to reset tables
-  .then(() => console.log('Database synced'))
-  .catch(err => console.error('Sync error:', err));
+sequelize
+  .sync({ alter: true }) // change to { force: true } to reset tables
+  .then(() => console.log("Database synced"))
+  .catch((err) => console.error("Sync error:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
